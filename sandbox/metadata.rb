cd58c83e916bc7532e2acc56dd32ad2445dfe39f
Wataru Noguchi <wnoguchi.0727@gmail.com>
2013-11-03 09:56:33 +0900

    berks cookbook.

---
+++ sandbox/metadata.rb
@@ -0,0 +1,8 @@
+name             'sandbox'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures sandbox'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
