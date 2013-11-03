19903be2314aaff1c2a32787cd911c9ff1adfc12
Wataru Noguchi <wnoguchi.0727@gmail.com>
2013-11-03 16:39:12 +0900

    Remove child files.

--- sandbox/metadata.rb
+++
@@ -1,8 +0,0 @@
-name             'sandbox'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures sandbox'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
-
